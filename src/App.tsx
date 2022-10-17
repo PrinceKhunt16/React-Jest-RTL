57ff3b946d7c08c76996830932c2aaf605e032a6
Prince Khunt <princekhuntdatastructure@gmail.com>
2022-10-17 11:34:23 +0530

    Mock in msw

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import "./App.css";
 import Application from "./components/application";
 import Counter from "./components/counter";
 import Skills from "./components/skills";
+import CounterTwo from "./components/counterTwo";
+import Users from "./components/users";
 import MuiMode from "./components/mui";
 import AppProviders from "./providers/AppProviders";
-import CounterTwo from "./components/counterTwo";
 
 const App = () => {
     return (
@@ -14,7 +15,8 @@ const App = () => {
                 {/* <Skills skills={['html', 'css', 'javascript']} /> */}
                 {/* <Counter /> */}
                 {/* <MuiMode /> */}
-                <CounterTwo count={1} />
+                {/* <CounterTwo count={1} /> */}
+                <Users />
             </div>
         </AppProviders>
     )
